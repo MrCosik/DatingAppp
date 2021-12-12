e692a0b972ef4ddd5606529c5e2989fa8e56eaa8
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-12-12 20:14:31 +0100

    Db migration done and controllers

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -28,9 +28,9 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.Find();
+            return await _context.Users.FindAsync(id);
         }
     }
 }
