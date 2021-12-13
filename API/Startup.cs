1615f5018e409573c4a5366b67338d0299c2f32f
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-12-13 21:34:57 +0100

    client wireframe done

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,7 +22,7 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-            
+
         }
 
 
@@ -30,7 +30,8 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<DataContext>(options => {
+            services.AddDbContext<DataContext>(options =>
+            {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
@@ -38,6 +39,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +56,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
